2b67d3fc45529a27f0fde5c822eb617edf907271
Mathis Richter <mathis.richter@ini.rub.de>
2011-02-21 22:31:37 +0100

    Added some gauss stuff.

--- DynamicField.py
+++ DynamicField.py
@@ -1,13 +1,27 @@
 import math
 import random
+import numpy
 
 def sigmoid(x, beta, x0):
     return 1./ (1. + math.exp(-beta * (x - x0)))
 
-class Node:
-    "Dynamical field"
+def gauss(x):
+    return 1. / sqrt(2. * PI) * exp(-1./2. * x**2)
 
-    def __init__(self, unique_id):
+def discrete_gauss(x, array_size):
+    gauss_array = zeros(array_size)
+
+    for i in range(array_size):
+        j = i - array_size / (array_size * 10)
+        gauss_array[i] = gauss(j)
+
+    return gauss_array
+
+
+class DynamicField:
+    "Dynamic field"
+
+    def __init__(self, dimension_sizes, unique_id):
         "Constructor"
 
         # seed the random number generator to have pseudo-random noise
@@ -16,31 +30,40 @@ class Node:
         # name of the field
         self._name = str('')
 
+        # dimensionality of the field
+        # TODO check that dimensionality is correct ( 0 <= dim <= 3 )
+        self._dimensionality = dimensionality
+
         # unique id of the field
         self._id = unique_id
-        
+         
         # amount of self excitation of the system
-        self._self_excitation = 5.0
-        # noise of the system
-        self._noise = 0.05
+        self._lateral_interaction = numpy.zeros(dimension_sizes) # TODO: convert to tensor
+        for 
+
+
+        # noise strength of the system
+        self._noise_strength = 0.05
         # value the activation will relax to without external input
         self._resting_level = -5.0
-        # input that comes from outside the system
-        self._external_input = 0.0
+        # input that comes from outside the system and can be used to drive the
+        # field through the detection instability
+        self._boost = 0.0
         # current value of the system (initialize it with the resting level,
         # because the field would relax to it without external input anyway)
-        self._current_value = self._resting_level
+        self._activation = numpy.zeros(dimension_sizes) + self._resting_level
         # controls how fast the system relaxes
-        self._time_scale = 20.0
+        self._relaxation_time = 20.0
         # controls the steepness of the sigmoid (nonlinearity) at the zero
         # crossing
         self._sigmoid_steepness = 5.0
         # controls the shift of the nonlinearity on the x-axis (sigmoid)
         self._sigmoid_shift = 0.0
-        # list of connections that come into the field
-        self._incident_connections = []
-        # list connections that go out from the field
-        self._adjacent_connections = []
+        # list of connectors that come into the field
+        self._incident_connectors = []
+        # list of connectors that go out from the field
+        self._adjacent_connectors = []
+
 
     def get_name(self):
         return self._name
@@ -48,20 +71,26 @@ class Node:
     def set_name(self, name):
         self._name = name
 
+    def get_dimensionality(self):
+        return self._dimensionality
+
+    def get_dimensionality_sizes(self):
+        return self._dimensionality_sizes
+
     def get_id(self):
         return self._id
 
-    def get_self_excitation(self):
-        return self._self_excitation
+    def get_lateral_interaction(self):
+        return self._lateral_interaction
 
-    def set_self_excitation(self, self_excitation):
-        self._self_excitation = self_excitation
+    def set_lateral_interaction(self, lateral_interaction):
+        self._lateral_interaction = lateral_interaction
 
-    def get_noise(self):
-        return self._noise
+    def get_noise_strength(self):
+        return self._noise_strength
 
-    def set_noise(self, noise):
-        self._noise = noise
+    def set_noise_strength(self, noise_strength):
+        self._noise_strength = noise_strength
 
     def get_resting_level(self):
         return self._resting_level
@@ -69,20 +98,20 @@ class Node:
     def set_resting_level(self, resting_level):
         self._resting_level = resting_level
 
-    def get_external_input(self):
-        return self._external_input
+    def get_boost(self):
+        return self._boost
 
-    def set_external_input(self, external_input):
-        self._external_input = external_input
+    def set_boost(self, boost):
+        self._boost = boost
 
-    def get_current_value(self):
-        return self._current_value
+    def get_activation(self):
+        return self._activation
 
-    def get_time_scale(self):
-        return self._time_scale
+    def get_relaxation_time(self):
+        return self._relaxation_time
 
-    def set_time_scale(self, time_scale):
-        self._time_scale = time_scale
+    def set_relaxation_time(self, relaxation_time):
+        self._relaxation_time = relaxation_time
 
     def get_sigmoid_steepness(self):
         return self._sigmoid_steepness
@@ -96,50 +125,51 @@ class Node:
     def set_sigmoid_shift(self, shift):
         self._sigmoid_shift = shift
 
-    def get_incident_connections(self):
-        return self._incident_connections
+    def get_incident_connectors(self):
+        return self._incident_connectors
 
-    def get_adjacent_connections(self):
-        return self._adjacent_connections
+    def get_adjacent_connectors(self):
+        return self._adjacent_connectors
 
-    def get_output(self, current_value=None):
+    def get_output(self, activation=None):
         """Compute the output of the field. By default, the current value of the
         field is used, but a different value can be supplied to compute the
         output for an arbitrary value."""
         # if the current value is not supplied..
-        if current_value is None:
-            # ..set it to the current value of the field
-            current_value = self._current_value
+        if activation is None:
+            # ..set it to the current activation of the field
+            activation = self._activation
 
-        return sigmoid(current_value, self._sigmoid_steepness, self._sigmoid_shift)
+        return sigmoid(activation, self._sigmoid_steepness, self._sigmoid_shift)
 
-    def get_change(self, current_value=None, use_time_scale=True):
+    def get_change(self, activation=None, use_time_scale=True):
         """Compute the next change to the system. By default, the current value
         of the field is used, but a different value can be supplied to compute
         the output for an arbitrary value. When use_time_scale is set to False,
         the time scale is not considered when computing the change."""
         # if the current value is not supplied..
-        if current_value is None:
+        if activation is None:
             # ..set it to the current value of the field
-            current_value = self._current_value
+            activation = self._activation
 
         # if the time scale is to be used..
         if use_time_scale is True:
             # ..compute the inverse of the time scale to have a factor..
-            time_scale_factor = 1. / self._time_scale
+            relaxation_time_factor = 1. / self._relaxation_time
         else:
             # ..otherwise, set the factor to one
-            time_scale_factor = 1.
+            relaxation_time_factor = 1.
 
         # sum up the input coming in from all connected fields
         field_interaction = 0
-        for connection in self._incident_connections:
-            field_interaction += connection.get_weight() * connection.get_source_field().get_output()
+        for connector in self._incident_connectors:
+            field_interaction += connector.get_output()
+
         # compute the change of the system
-        change = time_scale_factor * (- current_value
+        change = time_scale_factor * (- activation
                      + self._resting_level
-                     + self._external_input
-                     + self._self_excitation * self.get_output(current_value)
+                     + self._boost
+                     + numpy.convolve(self._lateral_interaction, self.get_output(activation), mode='same')
                      + field_interaction)
 
         return change
@@ -147,11 +177,11 @@ class Node:
     def step(self):
         """Compute the current change of the system and change to current value
         accordingly."""
-        self._current_value += self.get_change(self._current_value) + self._noise * (random.random() - 0.5)
+        self._activation += self.get_change(self._activation) + self._noise * (random.random() - 0.5)
 
 
-class Connection:
-    "Weighted, directed connection between two dynamical fields"
+class Connector:
+    "Directed connection between two dynamical fields"
 
     def __init__(self, source_field, target_field, weight, unique_id):
         "Constructor"
