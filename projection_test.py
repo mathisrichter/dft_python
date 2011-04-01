b8184ea85fa8201e08932c28fd4b02b4f71de359
Mathis Richter <mathis.richter@ini.rub.de>
2011-04-01 07:40:39 +0200

    Extended the projection test with an input.

--- projection_test.py
+++ projection_test.py
@@ -1,5 +1,6 @@
 import DynamicField
 import Kernel
+import numpy
 
 def main():
     interaction_kernel_0 = Kernel.GaussKernel(3)
@@ -12,9 +13,14 @@ def main():
     interaction_kernel_1.add_mode(-5.5, [5.5], [0.0])
     interaction_kernel_1.calculate()
 
-    field_0 = DynamicField.DynamicField([[5],[2],[10]], [], None)
-    field_0.set_boost(20)
-    field_1 = DynamicField.DynamicField([[5],[3],[4]], [], None)
+    input_field = DynamicField.DynamicField([[5],[2]], [], None)
+    input_field.set_boost(20)
+    blub = numpy.array([[0.0,0.5,1.0,0.5,0.0],[0.0,0.25,0.5,0.25,0.0]])
+    blub = blub.transpose() * 40 
+    input_weight = DynamicField.Weight(blub)
+
+    field_0 = DynamicField.DynamicField([[5],[2]], [], None)
+    field_1 = DynamicField.DynamicField([[3],[4]], [], None)
 
 
     scaler = DynamicField.Scaler()
@@ -22,28 +28,30 @@ def main():
 #    weight = DynamicField.Weight([0., 0.5, 1.0, 0.5, 0.0])
     weight = DynamicField.Weight(5.)
 
-    projection_0 = DynamicField.Projection(3, 0, set([]), [])
-    projection_1 = DynamicField.Projection(0, 3, set([]), [])
+    projection_0 = DynamicField.Projection(2, 2, set([0,1]), [1,0])
 
-    processing_steps = [projection_0, scaler, weight, projection_1]
+    processing_steps = [projection_0, scaler, weight]
     
     DynamicField.connect(field_0, field_1, processing_steps)
+    DynamicField.connect(input_field, field_0, [input_weight])
 
 
-#    for i in range(0, 500):
-    field_0.step()
-    print("field 0:")
-    print(field_0.get_output())
-    print(field_0.get_output().shape)
-    for processing_step in processing_steps:
-        processing_step.step()
-        print(processing_step.get_name() + ": ")
-        print(processing_step.get_output())
-        print(processing_step.get_output().shape)
-    field_1.step()
-    print("field 1:")
-    print(field_1.get_activation())
-    print(field_1.get_activation().shape)
+    for i in range(0, 500):
+        input_field.step()
+        input_weight.step()
+        field_0.step()
+        print("field 0:")
+        print(field_0.get_output())
+        print(field_0.get_output().shape)
+        for processing_step in processing_steps:
+            processing_step.step()
+            print(processing_step.get_name() + ": ")
+            print(processing_step.get_output())
+            print(processing_step.get_output().shape)
+        field_1.step()
+        print("field 1:")
+        print(field_1.get_activation())
+        print(field_1.get_activation().shape)
 
 if __name__ == "__main__":
     main()
